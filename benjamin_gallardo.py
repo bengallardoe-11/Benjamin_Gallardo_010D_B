0cd428617849e3805a5505b15b2f604f7c24c568
ben_gallardo_943 <ben.gallardoe@duocuc.cl>
2026-07-10 09:57:34 -0400

    se añadio la opcion 1 y 2 del enunciado

---
+++ benjamin_gallardo.py
@@ -0,0 +1,16 @@
+def buscar_codigo(codigo,diccionario):
+
+def menu():
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+
+
+def validar_nombre(nombre):return len(nombre)>0
+def validar_cantidad(cantidad):return cantidad>0
